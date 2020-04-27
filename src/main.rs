e51a811682a068a473f0ab523962e2dfd1ad2cda
shockron22 <jake@mccoy.engineer>
2020-04-27 11:22:28 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 mod openssl_encrypt;
 
 use openssl_encrypt::OpensslEncrypt;
+use openssl::symm::{Cipher};
 
 static ONE_KB: usize = 1024;
 
@@ -12,25 +13,47 @@ fn main() {
     let max_buffer_size = ONE_KB;
     let mut file_chunk_buf = vec![0u8; max_buffer_size as usize];
 
-    let mut file = File::open("test.txt").unwrap();
+    let mut file = File::open("test2.txt").unwrap();
 
     let file_metadata = file.metadata().unwrap();
-    let file_length = file_metadata.len();
+    let _file_length = file_metadata.len();
 
-    file.read_exact(&mut file_chunk_buf).unwrap();
-
-
+    
     let mut out_file = File::create("out.enc").unwrap();
+    
+    let mut openssl_encrypt = OpensslEncrypt::new("password".to_string(), Cipher::aes_256_cbc(), 10000);
 
-      file.read_exact(&mut file_chunk_buf).unwrap(); // will not work if file is too small. TODO
+    let mut out_file2 = File::create("out2.txt").unwrap();
 
-      // encrypt here
-        let openssl_encrypt = OpensslEncrypt::new("password".to_string(), 10000);
+    file.read(&mut file_chunk_buf).unwrap();
+    let encrypted_data = openssl_encrypt.encrypt(file_chunk_buf);
+    out_file.write(&encrypted_data).unwrap();
+    out_file.flush().unwrap();
+    let plaintext_data = openssl_encrypt.decrypt(encrypted_data);
+    out_file2.write(&plaintext_data).unwrap();
+    out_file2.flush().unwrap();
 
-        let out_text: Vec<u8> = openssl_encrypt.encrypt(file_chunk_buf);
-     //   println!("{:?}", out_text.iter());
+/*
+   loop {
+      let bytes_read = file.read(&mut file_chunk_buf).unwrap();
 
+      if bytes_read == 0 {
+        break;
+      }
+
+      let out_text: Vec<u8> = openssl_encrypt.encrypt_chunk(&file_chunk_buf);
       out_file.write(out_text.as_ref()).unwrap();
       out_file.flush().unwrap();
-    
+    }
+    let final_block: Vec<u8> = openssl_encrypt.finalize();
+    out_file.write(final_block.as_ref()).unwrap();
+    out_file.flush().unwrap();
+
+    let mut whole_file = vec![0u8; max_buffer_size * 2];
+    let bytes_read = file.read(&mut whole_file).unwrap();
+    println!("whole file bytes read: {}", bytes_read);
+    println!("{:?}", whole_file);
+    let decrypted_data = openssl_encrypt.decrypt(whole_file);
+    out_file3.write(&decrypted_data).unwrap();
+    out_file3.flush().unwrap();*/
 }
--- src/openssl_encrypt.rs
+++ src/openssl_encrypt.rs
@@ -1,76 +1,103 @@
-use openssl::symm::{Cipher, Mode, Crypter};
+use openssl::symm::{encrypt, decrypt, Cipher};
+
 use openssl::rand::rand_bytes;
 
 use ring::{pbkdf2};
 use std::{num::NonZeroU32};
 
 static PBKDF2_ALG: pbkdf2::Algorithm = pbkdf2::PBKDF2_HMAC_SHA256;
-static ONE_KB: usize = 1024;
 
 pub struct OpensslEncrypt {
-    pub magic_header: Vec<u8>,
-    pub encrypter: Crypter,
+    key: Vec<u8>,
+    iv: Vec<u8>,
+    magic_header: Vec<u8>,
+    cipher: Cipher,
 }
 
-impl OpensslEncrypt {
+trait GetRandomBytes {
     fn get_random_bytes(length: usize) -> Vec<u8> {
         let mut buf = vec![0; length];
         rand_bytes(&mut buf).unwrap();
         return buf;
     }
-    pub fn new(password: String, iteration_count: u32) -> OpensslEncrypt {
-        let passwordVec = password.as_bytes().to_vec();
+}
+
+impl GetRandomBytes for OpensslEncrypt {}
+
+impl OpensslEncrypt {
+    fn get_iv_salt_key(password: String, iteration_count: u32 ) -> (Vec<u8>, Vec<u8>, Vec<u8>) {
+        let password_vec = password.as_bytes().to_vec();
         let iterations = NonZeroU32::new(Some(iteration_count).unwrap_or(10000)).unwrap();
         let salt = OpensslEncrypt::get_random_bytes(8);
         let mut pbkdf2_key_iv = [0; 48]; // 256 bits + 128 bits
         
-        pbkdf2::derive(PBKDF2_ALG, iterations,  &salt, &passwordVec, &mut pbkdf2_key_iv);
+        pbkdf2::derive(PBKDF2_ALG, iterations,  &salt, &password_vec, &mut pbkdf2_key_iv);
 
         let key = pbkdf2_key_iv[0..32].to_vec(); // 256 bits
         let iv = pbkdf2_key_iv[32..48].to_vec(); // 128 bits
-
-        let magic_header = ["Salted__".as_bytes(), &salt].concat();
-        let encrypter = Crypter::new(
-            Cipher::aes_256_cbc(),
-            Mode::Encrypt,
-            &key,
-            Some(&iv)).unwrap();
-
-        return OpensslEncrypt { magic_header: magic_header, encrypter: encrypter};
+        
+        return (key, iv, salt);
     }
-    pub fn encrypt(mut self, chunk: Vec<u8>) -> Vec<u8> {
-        let block_size = Cipher::aes_256_cbc().block_size();
-        let mut ciphertext = vec![0; chunk.len() + block_size];
+    pub fn new(password: String, cipher: Cipher, iteration_count: u32) -> OpensslEncrypt {
+        let (key, iv, salt) = OpensslEncrypt::get_iv_salt_key(password, iteration_count);
 
-        let mut count = self.encrypter.update(&chunk, &mut ciphertext).unwrap();
-        count += self.encrypter.finalize(&mut ciphertext[count..]).unwrap();
-        ciphertext.truncate(count);
+        // let block_size = cipher.block_size();
+        let magic_header = ["Salted__".as_bytes(), &salt].concat();
 
+        return OpensslEncrypt {
+            key, 
+            iv,
+            magic_header,
+            cipher, 
+        };
+    }
+    pub fn encrypt(&mut self, data: Vec<u8>) -> Vec<u8> {
+        let ciphertext = encrypt(
+            self.cipher,
+            &self.key,
+            Some(&self.iv),
+            &data).unwrap();
+        
         return [&self.magic_header[..], &ciphertext[..]].concat();
     }
+    pub fn decrypt(&mut self, data: Vec<u8>) -> Vec<u8> {
+        let data_without_magic_header = &data[16..];
+        return decrypt(
+            self.cipher,
+            &self.key,
+            Some(&self.iv),
+            &data_without_magic_header).unwrap();
+    }
+}
 
-   /* pub fn decrypt(mut self, chunk: Vec<u8>) -> Vec<u8> {
-        // Let's pretend we don't know the plaintext, and now decrypt the ciphertext.
-        let data_len = ciphertext.len();
-        let ciphertexts = [&ciphertext[..9], &ciphertext[9..]];
 
-        // Create a cipher context for decryption.
-        let mut decrypter = Crypter::new(
-            Cipher::aes_128_cbc(),
-            Mode::Decrypt,
-            key,
-            Some(iv)).unwrap();
-        let mut plaintext = vec![0; data_len + block_size];
 
-        // Decrypt 2 chunks of ciphertexts successively.
-        let mut count = decrypter.update(ciphertexts[0], &mut plaintext).unwrap();
-        count += decrypter.update(ciphertexts[1], &mut plaintext[count..]).unwrap();
-        count += decrypter.finalize(&mut plaintext[count..]).unwrap();
-        plaintext.truncate(count);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        let mut out_file = File::create("out.txt").unwrap();
-        out_file.write(&plaintext).unwrap();
-        out_file.flush().unwrap();
-        //assert_eq!(b"Some Stream of Crypto Text", &plaintext[..]);
-    }*/
+    // mock/override default get_random_bytes to return static value
+    impl super::GetRandomBytesTest for OpensslEncrypt {
+        fn get_random_bytes(length: usize) -> Vec<u8> {
+            return b"\x53\x61\x23\x11\x23\x56\x74\x12\x34\x12\x23\x23\x23\x23\x54"[..length].to_vec();
+        }
+    }
+
+    #[test]
+    fn can_encrypt_correctly() {
+        let mut openssl_encrypt = OpensslEncrypt::new("password".to_string(), Cipher::aes_256_cbc(), 10000);
+        let encrypted_data = openssl_encrypt.encrypt("some data".as_bytes().to_vec());
+        assert_eq!(
+            b"\x53\x61\x6c\x74\x65\x64\x5f\x5f\x53\x61\x23\x11\x23\x56\x74\x12\x72\x30\x32\x8f\xca\x92\x3c\x3b\x53\x99\x11\x99\x14\x32\x79\x78".to_vec(),
+            encrypted_data
+        );
+    }
+    #[test]
+    fn can_decrypt_correctly() {
+        let mut openssl_encrypt = OpensslEncrypt::new("password".to_string(), Cipher::aes_256_cbc(), 10000);
+        let encrypted_data = b"\x53\x61\x6c\x74\x65\x64\x5f\x5f\x53\x61\x23\x11\x23\x56\x74\x12\x72\x30\x32\x8f\xca\x92\x3c\x3b\x53\x99\x11\x99\x14\x32\x79\x78".to_vec();
+        let decrypted_data = openssl_encrypt.decrypt(encrypted_data);
+
+        assert_eq!(b"some data", &decrypted_data[..]);
+    }
 }
